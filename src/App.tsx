013cbc775dd02c8de892f5e6af4e9c7b17d17988
divyuk <div14kes@gmail.com>
2023-07-26 17:31:33 +0530

    Navigation and Anchor Links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import NavBar from "@/scenes/navbar";
+import { useState } from "react";
 function App() {
-  return <div>App</div>;
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+  return (
+    <div className=" app bg-gray-20">
+      <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         "secondary-400": "#FFCD5B",
         "secondary-500": "#FFC132",
       },
-      backgroundImage: (theme) => ({
+      backgroundImage: () => ({
         "gradient-yellowred":
           "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
         "mobile-home": "url('./assets/HomePageGraphic.png')",
